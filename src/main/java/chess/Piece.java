934ed099493c6ecae6bbe7e971c4e89dcd29eb35
Sami Akram <sameakram2@gmail.com>
2021-01-17 23:57:30 +0200

    Position Enum added

--- src/main/java/chess/Piece.java
+++ src/main/java/chess/Piece.java
@@ -1,5 +1,31 @@
 package chess;
 
-public class Piece {
+public abstract class Piece {
+	private static int weight;
+	private static boolean white;
+	private static boolean black;
+	private Position position;
+
+	public static int getWeight() {
+		return weight;
+	}
+
+	public static boolean isWhite() {
+		return white;
+	}
+
+	public static void setWhite(boolean white) {
+		Piece.white = white;
+		Piece.black = !white;
+	}
+
+	public static boolean isBlack() {
+		return black;
+	}
+
+	public static void setBlack(boolean black) {
+		Piece.black = black;
+		Piece.white = !black;
+	}
 
 }
