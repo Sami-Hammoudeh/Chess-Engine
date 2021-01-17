810bc6e00fdaef0f953cae0b393dfe79d0c9490d
Sami Akram <sameakram2@gmail.com>
2021-01-18 00:38:01 +0200

    Added serialVersionUID

--- src/main/java/chess/exception/ChessException.java
+++ src/main/java/chess/exception/ChessException.java
@@ -1,6 +1,7 @@
 package chess.exception;
 
 public class ChessException extends Exception {
+	private static final long serialVersionUID = 1L;
 	public final static String IllegalMove = "Illeagel Move";
 
 	public ChessException(String message) {
