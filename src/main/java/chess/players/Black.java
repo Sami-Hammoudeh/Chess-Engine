a65576eba276f23f80d8fcf19d4f7736c467cc46
Sami Akram <sameakram2@gmail.com>
2021-01-18 16:32:22 +0200

    Update position class

--- src/main/java/chess/players/Black.java
+++ src/main/java/chess/players/Black.java
@@ -8,30 +8,27 @@ public class Black extends Player {
 
 	@Override
 	public void fillPieces() {
-		try {
-			// Pawns
-			addPiece(new Pawn(new Position('A', 7), Color.BLACK));
-			addPiece(new Pawn(new Position('B', 7), Color.BLACK));
-			addPiece(new Pawn(new Position('C', 7), Color.BLACK));
-			addPiece(new Pawn(new Position('D', 7), Color.BLACK));
-			addPiece(new Pawn(new Position('E', 7), Color.BLACK));
-			addPiece(new Pawn(new Position('F', 7), Color.BLACK));
-			addPiece(new Pawn(new Position('G', 7), Color.BLACK));
-			addPiece(new Pawn(new Position('H', 7), Color.BLACK));
-			// Rooks
-			addPiece(new Rook(new Position('A', 8), Color.BLACK));
-			addPiece(new Rook(new Position('H', 8), Color.BLACK));
-			// Knights
-			addPiece(new Knight(new Position('B', 8), Color.BLACK));
-			addPiece(new Knight(new Position('G', 8), Color.BLACK));
-			// Bishops
-			addPiece(new Bishop(new Position('C', 8), Color.BLACK));
-			addPiece(new Bishop(new Position('F', 8), Color.BLACK));
-			// Queen
-			addPiece(new Queen(new Position('D', 8), Color.BLACK));
-			// King
-			addPiece(new King(new Position('E', 8), Color.BLACK));
-		} catch (ChessException e) {
-		}
+		// Pawns
+		addPiece(new Pawn(new Position('A', 7), Color.BLACK));
+		addPiece(new Pawn(new Position('B', 7), Color.BLACK));
+		addPiece(new Pawn(new Position('C', 7), Color.BLACK));
+		addPiece(new Pawn(new Position('D', 7), Color.BLACK));
+		addPiece(new Pawn(new Position('E', 7), Color.BLACK));
+		addPiece(new Pawn(new Position('F', 7), Color.BLACK));
+		addPiece(new Pawn(new Position('G', 7), Color.BLACK));
+		addPiece(new Pawn(new Position('H', 7), Color.BLACK));
+		// Rooks
+		addPiece(new Rook(new Position('A', 8), Color.BLACK));
+		addPiece(new Rook(new Position('H', 8), Color.BLACK));
+		// Knights
+		addPiece(new Knight(new Position('B', 8), Color.BLACK));
+		addPiece(new Knight(new Position('G', 8), Color.BLACK));
+		// Bishops
+		addPiece(new Bishop(new Position('C', 8), Color.BLACK));
+		addPiece(new Bishop(new Position('F', 8), Color.BLACK));
+		// Queen
+		addPiece(new Queen(new Position('D', 8), Color.BLACK));
+		// King
+		addPiece(new King(new Position('E', 8), Color.BLACK));
 	}
 }
--- src/main/java/chess/players/White.java
+++ src/main/java/chess/players/White.java
@@ -8,30 +8,27 @@ public class White extends Player {
 
 	@Override
 	public void fillPieces() {
-		try {
-			// Pawns
-			addPiece(new Pawn(new Position('A', 2), Color.WHITE));
-			addPiece(new Pawn(new Position('B', 2), Color.WHITE));
-			addPiece(new Pawn(new Position('C', 2), Color.WHITE));
-			addPiece(new Pawn(new Position('D', 2), Color.WHITE));
-			addPiece(new Pawn(new Position('E', 2), Color.WHITE));
-			addPiece(new Pawn(new Position('F', 2), Color.WHITE));
-			addPiece(new Pawn(new Position('G', 2), Color.WHITE));
-			addPiece(new Pawn(new Position('H', 2), Color.WHITE));
-			// Rooks
-			addPiece(new Rook(new Position('A', 1), Color.WHITE));
-			addPiece(new Rook(new Position('H', 1), Color.WHITE));
-			// Knights
-			addPiece(new Knight(new Position('B', 1), Color.WHITE));
-			addPiece(new Knight(new Position('G', 1), Color.WHITE));
-			// Bishops
-			addPiece(new Bishop(new Position('C', 1), Color.WHITE));
-			addPiece(new Bishop(new Position('F', 1), Color.WHITE));
-			// Queen
-			addPiece(new Queen(new Position('D', 1), Color.WHITE));
-			// King
-			addPiece(new King(new Position('E', 1), Color.WHITE));
-		} catch (ChessException e) {
-		}
+		// Pawns
+		addPiece(new Pawn(new Position('A', 2), Color.WHITE));
+		addPiece(new Pawn(new Position('B', 2), Color.WHITE));
+		addPiece(new Pawn(new Position('C', 2), Color.WHITE));
+		addPiece(new Pawn(new Position('D', 2), Color.WHITE));
+		addPiece(new Pawn(new Position('E', 2), Color.WHITE));
+		addPiece(new Pawn(new Position('F', 2), Color.WHITE));
+		addPiece(new Pawn(new Position('G', 2), Color.WHITE));
+		addPiece(new Pawn(new Position('H', 2), Color.WHITE));
+		// Rooks
+		addPiece(new Rook(new Position('A', 1), Color.WHITE));
+		addPiece(new Rook(new Position('H', 1), Color.WHITE));
+		// Knights
+		addPiece(new Knight(new Position('B', 1), Color.WHITE));
+		addPiece(new Knight(new Position('G', 1), Color.WHITE));
+		// Bishops
+		addPiece(new Bishop(new Position('C', 1), Color.WHITE));
+		addPiece(new Bishop(new Position('F', 1), Color.WHITE));
+		// Queen
+		addPiece(new Queen(new Position('D', 1), Color.WHITE));
+		// King
+		addPiece(new King(new Position('E', 1), Color.WHITE));
 	}
 }
