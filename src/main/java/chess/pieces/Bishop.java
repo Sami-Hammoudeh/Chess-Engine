ab4f3412790d9f6dfe81c713fa37ec3a7a7ca767
Sami Akram <sameakram2@gmail.com>
2021-01-18 22:58:04 +0200

    Added Bishop legal moves

--- src/main/java/chess/pieces/Bishop.java
+++ src/main/java/chess/pieces/Bishop.java
@@ -2,6 +2,7 @@ package chess.pieces;
 
 import java.util.LinkedList;
 
+import chess.board.Board;
 import chess.board.Position;
 import chess.players.Color;
 
@@ -14,7 +15,55 @@ public class Bishop extends Piece {
 
 	@Override
 	public LinkedList<Position> getLegalMoves() {
-		return null;
+		legalMoves = new LinkedList<Position>();
+		Position position;
+		// First Direction
+		for (int i = 1; i < 8; i++) {
+			position = getPosition().addRows(i).addColums(i);
+			if (Board.isEmpty(position)) {
+				addLegalMove(position);
+			} else {
+				if (Board.isEnemy(position, getColor())) {
+					addLegalMove(position);
+				}
+				break;
+			}
+		}
+		for (int i = 7; i > 0; i--) {
+			position = getPosition().addRows(i).addColums(i);
+			if (Board.isEmpty(position)) {
+				addLegalMove(position);
+			} else {
+				if (Board.isEnemy(position, getColor())) {
+					addLegalMove(position);
+				}
+				break;
+			}
+		}
+		// Second Direction
+		for (int i = 1; i < 8; i++) {
+			position = getPosition().addColums(i).addRows(-i);
+			if (Board.isEmpty(position)) {
+				addLegalMove(position);
+			} else {
+				if (Board.isEnemy(position, getColor())) {
+					addLegalMove(position);
+				}
+				break;
+			}
+		}
+		for (int i = 7; i > 0; i--) {
+			position = getPosition().addColums(i).addRows(-i);
+			if (Board.isEmpty(position)) {
+				addLegalMove(position);
+			} else {
+				if (Board.isEnemy(position, getColor())) {
+					addLegalMove(position);
+				}
+				break;
+			}
+		}
+		return legalMoves;
 	}
 
 }
