3bc379406cec00ad9f25654217b54d3da1d8dd6b
Sami Akram <sameakram2@gmail.com>
2021-01-18 23:13:59 +0200

    Added initial King legal moves

--- src/main/java/chess/pieces/King.java
+++ src/main/java/chess/pieces/King.java
@@ -2,10 +2,11 @@ package chess.pieces;
 
 import java.util.LinkedList;
 
+import chess.board.Board;
 import chess.board.Position;
 import chess.players.Color;
 
-public class King extends Piece{
+public class King extends Piece {
 
 	public King(Position position, Color color) {
 		super(position, color);
@@ -13,7 +14,18 @@ public class King extends Piece{
 
 	@Override
 	public LinkedList<Position> getLegalMoves() {
-		return null;
+		legalMoves = new LinkedList<Position>();
+		// Columns
+		int c[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+		// Rows
+		int r[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+		for (int i = 0; i < c.length; i++) {
+			Position position = getPosition().addRows(r[i]).addColums(c[i]);
+			if (Board.isEmpty(position) || Board.isEnemy(position, getColor())) {
+				addLegalMove(position);
+			}
+		}
+		return legalMoves;
 	}
 
 }
