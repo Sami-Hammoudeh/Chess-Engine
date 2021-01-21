456f8bf406ae8db22e91487b594593fdfc661292
Sami Akram <sameakram2@gmail.com>
2021-01-21 02:27:30 +0200

    Added Chess.collection

---
+++ src/main/java/chess/collection/ChessHashSet.java
@@ -0,0 +1,10 @@
+package chess.collection;
+
+import java.util.HashSet;
+
+public class ChessHashSet<E> extends HashSet<E> {
+
+	private static final long serialVersionUID = 1L;
+
+	
+}
---
+++ src/main/java/chess/collection/ChessLinkedList.java
@@ -0,0 +1,9 @@
+package chess.collection;
+
+import java.util.LinkedList;
+
+public class ChessLinkedList<E> extends LinkedList<E> {
+
+	private static final long serialVersionUID = 1L;
+
+}
