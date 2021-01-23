2dc2330ef51a508bfcec88dd61832b48c3e25c42
Sami Akram <sameakram2@gmail.com>
2021-01-23 21:55:04 +0200

    Added final objects

--- src/main/java/chess/control/Main.java
+++ src/main/java/chess/control/Main.java
@@ -1,9 +1,13 @@
 package chess.control;
 
 import chess.board.Board;
+import chess.players.Black;
+import chess.players.White;
 
 public class Main {
-	public final static Board mainBoard = new Board();
+	public final static Board BOARD = new Board();
+	public final static White WHITE = new White();
+	public final static Black BLACK = new Black();
 
 	public static void main(String[] args) {
 		
