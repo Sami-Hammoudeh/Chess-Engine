0ca02b7276c2e1ca12ef0a9b2ca98b1409cc87e7
Sami Akram <sameakram2@gmail.com>
2021-01-23 21:22:14 +0200

    Implemented equals & copy methods

--- src/main/java/chess/collections/ChessHashSet.java
+++ src/main/java/chess/collections/ChessHashSet.java
@@ -1,6 +1,7 @@
 package chess.collections;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 import chess.object.ChessObject;
 
@@ -8,14 +9,29 @@ public class ChessHashSet<E extends ChessObject> extends HashSet<ChessObject> im
 
 	private static final long serialVersionUID = 1L;
 
-	public boolean equals(ChessCollection<ChessObject> chessCollection) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean equals(ChessCollection<ChessObject> list) {
+		if (list == this)
+			return true;
+		if (list == null || list.getClass() != getClass())
+			return false;
+		ChessHashSet<ChessObject> linkedList = (ChessHashSet<ChessObject>) list;
+		if (linkedList.size() != size())
+			return false;
+		Iterator<ChessObject> i1 = linkedList.iterator();
+		Iterator<ChessObject> i2 = iterator();
+		while (i1.hasNext() && i2.hasNext())
+			if (!(i1.next().equals(i2.next())))
+				return false;
+		return true;
 	}
 
 	public ChessCollection<ChessObject> copy() {
-		// TODO Auto-generated method stub
-		return null;
+		ChessHashSet<ChessObject> list = new ChessHashSet<ChessObject>();
+		Iterator<ChessObject> iterator = iterator();
+		while (iterator.hasNext()) {
+			list.add(iterator.next());
+		}
+		return list;
 	}
 
 }
--- src/main/java/chess/collections/ChessLinkedList.java
+++ src/main/java/chess/collections/ChessLinkedList.java
@@ -1,20 +1,37 @@
 package chess.collections;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import chess.object.ChessObject;
 
-public class ChessLinkedList<E extends ChessObject> extends LinkedList<ChessObject> implements ChessCollection<ChessObject> {
+public class ChessLinkedList<E extends ChessObject> extends LinkedList<ChessObject>
+		implements ChessCollection<ChessObject> {
 
 	private static final long serialVersionUID = 1L;
 
-	public boolean equals(ChessCollection<ChessObject> chessCollection) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean equals(ChessCollection<ChessObject> list) {
+		if (list == this)
+			return true;
+		if (list == null || list.getClass() != getClass())
+			return false;
+		ChessLinkedList<ChessObject> linkedList = (ChessLinkedList<ChessObject>) list;
+		if (linkedList.size() != size())
+			return false;
+		Iterator<ChessObject> i1 = linkedList.iterator();
+		Iterator<ChessObject> i2 = iterator();
+		while (i1.hasNext() && i2.hasNext())
+			if (!(i1.next().equals(i2.next())))
+				return false;
+		return true;
 	}
 
 	public ChessCollection<ChessObject> copy() {
-		// TODO Auto-generated method stub
-		return null;
+		ChessLinkedList<ChessObject> list = new ChessLinkedList<ChessObject>();
+		Iterator<ChessObject> iterator = iterator();
+		while (iterator.hasNext()) {
+			list.add(iterator.next());
+		}
+		return list;
 	}
 
 }
